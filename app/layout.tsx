d6c1c66d11a287c2aa0d902a7d3c1d601e1b0f16
antozz996 <afiman96@gmail.com>
2026-09-08 12:36:50 +0200

    Optimize QR scanner layout for mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import './scanner-mobile.css';
 
 export const metadata: Metadata = {
   title: 'DAMAI Open Days 2026 | La Dolce Vita',
